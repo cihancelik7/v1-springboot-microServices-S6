c24d2a776009dc08bceebaee5656b7b9b19189ba
Cihan Celik <cihan.celik7@gmail.com>
2025-01-02 23:42:11 +0300

    spring cloud working

--- cards/src/main/java/com/cihancelik/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/cihancelik/cards/dto/CardsContactInfoDto.java
@@ -1,6 +1,7 @@
 package com.cihancelik.cards.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
